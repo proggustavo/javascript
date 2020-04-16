21c6c58bbdcdada469b191071a83ebaf27f1e8cb
proggustavo <prog.gustavosantos@gmail.com>
2020-04-16 19:49:50 -0300

    funções

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,11 +1,21 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const funcionarioChines = funcionario => funcionario.pais === 'China'
+const funcionariaMulher = funcionario => funcionario.genero === 'F'
+const menorSalario = (funcionario, funcionarioAtual) => {
+    return funcionario.salario < funcionarioAtual.salario ? funcionario : funcionarioAtual
+}
+
+
+// mulher chinesa com o menor salário
+
 axios.get(url).then(response => {
     const funcionarios = response.data
-    funcionarios.map((funcionario) => {
-        funcionario.
-    })
-    console.log(funcionarios)
+
+    const resultado = funcionarios.filter(funcionarioChines).filter(funcionariaMulher).reduce(menorSalario)
+    console.log(resultado)
 
 })
+
+
