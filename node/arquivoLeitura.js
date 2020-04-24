97035fcb245bf7f234b8f05f0bb60ef7817bf125
proggustavo <prog.gustavosantos@gmail.com>
2020-04-24 19:31:04 -0300

    callback e arquivo

---
+++ node/arquivoLeitura.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+const caminho = __dirname + '/arquivo.json'
+
+// sincrono...  
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+
+// assincrono ...
+
+fs.readFile(caminho, 'utf-8')
