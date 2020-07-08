4677ab45b7b68d3ed18a85e243513b23fdf2df25
proggustavo <prog.gustavosantos@gmail.com>
2020-07-08 00:06:53 -0300

    middleware e ordem de requisicoes

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,8 +1,43 @@
 const express = require('express')
 const app = express()
 
-app.get('/opa', (req, res) => {
-    res.send('estou bem')
+app.get('/opa', (req,  res, next) => {
+    console.log('Antes..')
+    next()
+})
+
+app.get('/opa', (req, res, next) => {
+    console.log('durante..')
+    res.json({
+        data: [
+            {id: 7, name:'Ana', position: 1},
+            {id: 34, name: 'Bia', position: 2},
+            {id: 73, name: 'Carlos', position:3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+    next()
+    // res.json([
+    //     {id: 7, name:'Ana', position: 1},
+    //     {id: 34, name: 'Bia', position: 2},
+    //     {id: 73, name: 'Carlos', position:3}
+    // ])
+
+
+    // res.json({
+    //     name: 'iPad 32Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+    // res.send('estou bem');
+})
+
+app.get('/opa', (req,  res, ) => {
+    console.log('Depois..')
+    
 })
 
 app.listen(3000,  () => {
