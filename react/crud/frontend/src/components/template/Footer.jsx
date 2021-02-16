7eaa0473532f3065d760bee177782605a28a0290
proggustavo <prog.gustavosantos@gmail.com>
2021-02-16 20:06:23 -0300

    iniciando projeto crud react

---
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,5 @@
+import "./Footer.css";
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => <footer className="footer">Rodape</footer>;
---
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -0,0 +1,5 @@
+import "./Nav.css";
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => <aside className="menu-area">Navegacao</aside>;
