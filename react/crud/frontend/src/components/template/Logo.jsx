89c69577f05e200f16dfadd7a5fe2a9a6281dcef
proggustavo <prog.gustavosantos@gmail.com>
2021-02-18 21:18:09 -0300

    iniciando projeto cadastro react

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -1,5 +1,12 @@
 import "./Logo.css";
 import React from "react";
+import logo from "../../assets/images/logo.png";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (props) => <aside className="logo">Logo</aside>;
+export default (props) => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -4,13 +4,19 @@ import Logo from "../components/template/Logo";
 import Nav from "../components/template/Nav";
 import Main from "../components/template/Main";
 import Footer from "../components/template/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => (
   <div className="app">
     <Logo />
     <Nav />
-    <Main />
+    <Main
+      icon="home"
+      title="Início"
+      subtitle="Segundo Projeto capitulo de React."
+    />
     <Footer />
   </div>
 );
