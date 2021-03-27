cd995d5aeb9ffed724779315dbf53684c465a26f
proggustavo <prog.gustavosantos@gmail.com>
2021-03-27 11:44:36 -0300

    Multer

---
+++ multer/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => res.render("home"));
+
+app.post("/", upload.single("img"), (req, res) => {
+  console.log(req.body, req.file);
+  res.send("ok");
+});
+
+app.listen(3000, () => console.log("Running.."));
